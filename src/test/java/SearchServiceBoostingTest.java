81c222a469bc76f841b0e7fcbc894d01dd9dc245
Sergey Borisenko <sborisenko@griddynamics.com>
2021-11-18 11:59:25 +0200

    Update SearchServiceBoostingTest sample

--- src/test/java/SearchServiceBoostingTest.java
+++ src/test/java/SearchServiceBoostingTest.java
@@ -24,75 +24,67 @@ import com.google.cloud.retail.v2.SearchRequest.BoostSpec.ConditionBoostSpec;
 import com.google.cloud.retail.v2.SearchResponse;
 import com.google.cloud.retail.v2.SearchServiceClient;
 import com.google.cloud.retail.v2.SearchServiceSettings;
-import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public class SearchServiceBoostingTest {
-    //TODO(developers) change the values of following variables to use proper project
-    private static final long YOUR_PROJECT_NUMBER = 1038874412926L;
 
-    private static final String ENDPOINT = "retail.googleapis.com:443";
-    private static final String DEFAULT_CATALOG_NAME =
-            String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
-    private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
-            DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final long YOUR_PROJECT_NUMBER = Long.parseLong(System.getenv("PROJECT_NUMBER"));
 
-    private static final String VISITOR_ID = UUID.randomUUID().toString();
+  private static final String ENDPOINT = "retail.googleapis.com";
+  private static final String DEFAULT_CATALOG_NAME =
+      String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
+  private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
+      DEFAULT_CATALOG_NAME + "/placements/default_search";
 
-    // get search service client
-    private static SearchServiceClient getSearchServiceClient() throws IOException {
-        SearchServiceSettings settings = SearchServiceSettings.newBuilder()
-                .setEndpoint(ENDPOINT)
-                .build();
-        return SearchServiceClient.create(settings);
-    }
+  private static final String VISITOR_ID = UUID.randomUUID().toString();
 
-    private static final String DEFAULT_BRANCH_NAME =
-            DEFAULT_CATALOG_NAME + "/branches/default_branch";
+  // get search service client
+  private static SearchServiceClient getSearchServiceClient() throws IOException {
+    SearchServiceSettings settings = SearchServiceSettings.newBuilder()
+        .setEndpoint(ENDPOINT)
+        .build();
+    return SearchServiceClient.create(settings);
+  }
 
-    // get search service request
-    public static SearchResponse searchProductsWithBoostSpec(String query, int pageSize,
-                                                             String condition, float boostStrength) throws IOException, InterruptedException {
-        SearchServiceClient searchClient = getSearchServiceClient();
+  // get search service request
+  public static SearchResponse searchProductsWithBoostSpec(String query,
+      String condition, float boostStrength) throws IOException {
+    SearchServiceClient searchClient = getSearchServiceClient();
 
-        BoostSpec boostSpec = BoostSpec.newBuilder()
-                .addConditionBoostSpecs(ConditionBoostSpec.newBuilder()
-                                                .setCondition(condition)
-                                                .setBoost(boostStrength)
-                                                .build())
-                .build();
+    BoostSpec boostSpec = BoostSpec.newBuilder()
+        .addConditionBoostSpecs(ConditionBoostSpec.newBuilder()
+            .setCondition(condition)
+            .setBoost(boostStrength)
+            .build())
+        .build();
 
-        SearchRequest searchRequest = SearchRequest.newBuilder()
-                .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME) // Placement is used to identify the Serving Config name.
-                .setBranch(DEFAULT_BRANCH_NAME)
-                .setVisitorId(VISITOR_ID) // A unique identifier to track visitors
-                .setQuery(query)
-                .setPageSize(pageSize)
-                .setBoostSpec(boostSpec)
-                .build();
-        System.out.println("Search with boosting specification, request: " + searchRequest);
-        SearchResponse response = searchClient.search(searchRequest).getPage().getResponse();
+    SearchRequest searchRequest = SearchRequest.newBuilder()
+        .setPlacement(
+            DEFAULT_SEARCH_PLACEMENT_NAME) // Placement is used to identify the Serving Config name.
+        .setQuery(query)
+        .setVisitorId(VISITOR_ID) // A unique identifier to track visitors
+        .setBoostSpec(boostSpec)
+        .build();
 
-        searchClient.shutdownNow();
-        searchClient.awaitTermination(2, TimeUnit.SECONDS);
-        System.out.println("Search with boosting specification, response: " + response);
-        return response;
-    }
+    System.out.println("Search with boosting specification, request: " + searchRequest);
+    SearchResponse response = searchClient.search(searchRequest).getPage().getResponse();
 
-    // call the Retail Search:
-    @Test
-    public void search() throws IOException, InterruptedException {
-        // TRY DIFFERENT CONDITIONS HERE:
-        searchProductsWithBoostSpec("Nest", 10, "(colorFamily: ANY(\"grey\"))", 0.5f);
+    System.out.println("Search with boosting specification, response: " + response);
+    return response;
+  }
 
-        List<String> variantRollupKeys = Lists
-                .newArrayList("colorFamily", "pickupInStore.store123");
-    }
+  // call the Retail Search:
+  @Test
+  public void search() throws IOException, InterruptedException {
+    // TRY DIFFERENT CONDITIONS HERE:
+    String condition = "(colorFamily: ANY(\"Blue\"))";
+    float boost = 0.0f;
+
+    searchProductsWithBoostSpec("Tee", condition, boost);
+  }
 }
 
 // [END retail_search_product_with_boost_spec]
