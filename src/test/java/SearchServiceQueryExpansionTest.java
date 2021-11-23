656a06b8c3349d1d691cb088e5402ae85bc3aed7
Sergey Borisenko <sborisenko@griddynamics.com>
2021-11-23 15:13:45 +0200

    Update 4 samples

--- src/test/java/SearchServiceQueryExpansionTest.java
+++ src/test/java/SearchServiceQueryExpansionTest.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * [START retail_search_for_products_with_query_expansion_specification]
+ * Call Retail API to search for a products in a catalog,
+ * enabling the query expansion feature to let the Google Retail Search build an automatic query expansion.
+ */
+
 import com.google.cloud.retail.v2.SearchRequest;
 import com.google.cloud.retail.v2.SearchRequest.QueryExpansionSpec;
 import com.google.cloud.retail.v2.SearchRequest.QueryExpansionSpec.Condition;
@@ -11,59 +31,58 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SearchServiceQueryExpansionTest {
-    //TODO(developers) change the values of following variables to use proper project
-    private static final long YOUR_PROJECT_NUMBER = 00000L;
 
-    private static final String ENDPOINT = "retail.googleapis.com:443";
-    private static final String DEFAULT_CATALOG_NAME =
-            String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
-    private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
-            DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final long YOUR_PROJECT_NUMBER = Long.parseLong(System.getenv("PROJECT_NUMBER"));
+  private static final String ENDPOINT = "retail.googleapis.com";
+  private static final String DEFAULT_CATALOG_NAME =
+      String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
+  private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
+      DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final String VISITOR_ID = UUID.randomUUID().toString();
 
-    private static final String VISITOR_ID = UUID.randomUUID().toString();
+  // get search service client
+  private static SearchServiceClient getSearchServiceClient() throws IOException {
+    SearchServiceSettings settings = SearchServiceSettings.newBuilder()
+        .setEndpoint(ENDPOINT)
+        .build();
+    return SearchServiceClient.create(settings);
+  }
 
-    // [START search_client]
-    private static SearchServiceClient getSearchServiceClient() throws IOException {
-        SearchServiceSettings settings = SearchServiceSettings.newBuilder()
-                .setEndpoint(ENDPOINT)
-                .build();
-        return SearchServiceClient.create(settings);
-    }
-    // [END search_client]
+  // get search service request
+  public static SearchResponse searchProductsWithQueryExpansion(String query, Condition condition)
+      throws IOException, InterruptedException {
+    SearchServiceClient searchClient = getSearchServiceClient();
 
-    private static final String DEFAULT_BRANCH_NAME =
-            DEFAULT_CATALOG_NAME + "/branches/default_branch";
+    QueryExpansionSpec queryExpansionSpec = QueryExpansionSpec.newBuilder()
+        .setCondition(condition)
+        .build();
 
-    // [START search_product_queryExpansion]
-    public static SearchResponse searchProductsWithQueryExpansion(String query, int pageSize,
-                                                                  Condition condition) throws IOException, InterruptedException {
-        SearchServiceClient searchClient = getSearchServiceClient();
+    SearchRequest searchRequest = SearchRequest.newBuilder()
+        .setPlacement(
+            DEFAULT_SEARCH_PLACEMENT_NAME) // Placement is used to identify the Serving Config name.
+        .setQuery(query)
+        .setVisitorId(VISITOR_ID) // A unique identifier to track visitors
+        .setQueryExpansionSpec(queryExpansionSpec)
+        .build();
+    System.out.println("Search with query expansion, request: " + searchRequest);
 
-        QueryExpansionSpec queryExpansionSpec = QueryExpansionSpec.newBuilder()
-                .setCondition(condition)
-                .build();
+    SearchResponse response = searchClient.search(searchRequest).getPage().getResponse();
 
-        SearchRequest searchRequest = SearchRequest.newBuilder()
-                .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
-                .setBranch(DEFAULT_BRANCH_NAME)
-                .setVisitorId(VISITOR_ID)
-                .setQuery(query)
-                .setPageSize(pageSize)
-                .setQueryExpansionSpec(queryExpansionSpec)
-                .build();
-        System.out.println("Search with query expansion, request: " + searchRequest);
-        SearchResponse response = searchClient.search(searchRequest).getPage().getResponse();
+    searchClient.shutdownNow();
+    searchClient.awaitTermination(2, TimeUnit.SECONDS);
 
-        searchClient.shutdownNow();
-        searchClient.awaitTermination(2, TimeUnit.SECONDS);
-        System.out.println("Search with query expansion, response: " + response);
-        return response;
-    }
-    // [END search_product_queryExpansion]
+    System.out.println("Search with query expansion, response: " + response);
+    return response;
+  }
 
-    @Test
-    public void search() throws IOException, InterruptedException {
+  // call the Retail Search
+  @Test
+  public void search() throws IOException, InterruptedException {
+    // TRY DIFFERENT QUERY EXPANSION CONDITION HERE:
+    Condition condition = Condition.AUTO;
 
-        searchProductsWithQueryExpansion("Nest_Maxi", 10, Condition.AUTO);
-    }
+    searchProductsWithQueryExpansion("Google Youth Hero Tee Grey", condition);
+  }
 }
+
+// [END retail_search_for_products_with_query_expansion_specification]
--- src/test/java/SearchServiceSimpleQueryTest.java
+++ src/test/java/SearchServiceSimpleQueryTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * [START retail_search_for_products_with_query_parameter]
+ * Call Retail API to search for a products in a catalog using only search query.
+ */
+
 import com.google.cloud.retail.v2.SearchRequest;
 import com.google.cloud.retail.v2.SearchResponse;
 import com.google.cloud.retail.v2.SearchServiceClient;
@@ -9,54 +28,49 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SearchServiceSimpleQueryTest {
-    //TODO(developers) change the values of following variables to use proper project
-    private static final long YOUR_PROJECT_NUMBER = 00000L;
 
-    private static final String ENDPOINT = "retail.googleapis.com:443";
-    private static final String DEFAULT_CATALOG_NAME =
-            String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
-    private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
-            DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final long YOUR_PROJECT_NUMBER = Long.parseLong(System.getenv("PROJECT_NUMBER"));
+  private static final String ENDPOINT = "retail.googleapis.com";
+  private static final String DEFAULT_CATALOG_NAME =
+      String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
+  private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
+      DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final String VISITOR_ID = UUID.randomUUID().toString();
 
-    private static final String VISITOR_ID = UUID.randomUUID().toString();
+  // get search service client
+  private static SearchServiceClient getSearchServiceClient() throws IOException {
+    SearchServiceSettings settings = SearchServiceSettings.newBuilder()
+        .setEndpoint(ENDPOINT)
+        .build();
+    return SearchServiceClient.create(settings);
+  }
 
-    // [START search_client]
-    private static SearchServiceClient getSearchServiceClient() throws IOException {
-        SearchServiceSettings settings = SearchServiceSettings.newBuilder()
-                .setEndpoint(ENDPOINT)
-                .build();
-        return SearchServiceClient.create(settings);
-    }
-    // [END search_client]
+  // get search service request
+  public static SearchResponse searchProducts(String query)
+      throws IOException, InterruptedException {
+    SearchServiceClient searchClient = getSearchServiceClient();
 
-    private static final String DEFAULT_BRANCH_NAME =
-            DEFAULT_CATALOG_NAME + "/branches/default_branch";
+    SearchRequest searchRequest = SearchRequest.newBuilder()
+        .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
+        .setQuery(query)
+        .setVisitorId(VISITOR_ID)
+        .build();
+    System.out.println("Search with only a query, request: " + searchRequest);
+    SearchResponse response = searchClient.search(searchRequest).getPage().getResponse();
 
-    // [START search_product]
-    public static SearchResponse searchProducts(String query) throws IOException, InterruptedException {
-        SearchServiceClient searchClient = getSearchServiceClient();
+    searchClient.shutdownNow();
+    searchClient.awaitTermination(2, TimeUnit.SECONDS);
 
-        SearchRequest searchRequest = SearchRequest.newBuilder()
-                .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
-                .setBranch(DEFAULT_BRANCH_NAME)
-                .setVisitorId(VISITOR_ID)
-                .setQuery(query)
-                .build();
-        System.out.println("Search with only a query, request: " + searchRequest);
-        SearchResponse response = searchClient.search(searchRequest).getPage().getResponse();
+    System.out.println("Search with only a query, response: " + response);
+    return response;
+  }
 
-        searchClient.shutdownNow();
-        searchClient.awaitTermination(2, TimeUnit.SECONDS);
+  // call the Retail Search
+  @Test
+  public void search() throws IOException, InterruptedException {
 
-        System.out.println("Search with only a query, response: " + response);
-        return response;
-    }
-    // [END search_product]
-
-
-    @Test
-    public void search() throws IOException, InterruptedException {
-
-        searchProducts("Nest_Maxi");
-    }
+    searchProducts("Hoodie");
+  }
 }
+
+// [END retail_search_for_products_with_query_parameter]
