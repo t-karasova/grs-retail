27fee54c4fce83cf1362dcabbc3e83ee1b378518
Sergey Borisenko <sborisenko@griddynamics.com>
2021-11-23 11:33:48 +0200

    Update 3 samples (pagination)

--- src/test/java/SearchServicePaginationNextPageTest.java
+++ src/test/java/SearchServicePaginationNextPageTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * [START retail_search_for_products_with_next_page]
+ * Call Retail API to search for a products in a catalog,
+ * limit the number of the products per page and go to the next page using "next_page_token"
+ * or jump to chosen page using "offset".
+ */
+
 import com.google.cloud.retail.v2.SearchRequest;
 import com.google.cloud.retail.v2.SearchResponse;
 import com.google.cloud.retail.v2.SearchServiceClient;
@@ -9,66 +30,64 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SearchServicePaginationNextPageTest {
-    //TODO(developers) change the values of following variables to use proper project
-    private static final long YOUR_PROJECT_NUMBER = 00000L;
 
-    private static final String ENDPOINT = "retail.googleapis.com:443";
-    private static final String DEFAULT_CATALOG_NAME =
-            String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
-    private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
-            DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final long YOUR_PROJECT_NUMBER = Long.parseLong(System.getenv("PROJECT_NUMBER"));
+  private static final String ENDPOINT = "retail.googleapis.com";
+  private static final String DEFAULT_CATALOG_NAME =
+      String.format("projects/%d/locations/global/catalogs/default_catalog", YOUR_PROJECT_NUMBER);
+  private static final String DEFAULT_SEARCH_PLACEMENT_NAME =
+      DEFAULT_CATALOG_NAME + "/placements/default_search";
+  private static final String VISITOR_ID = UUID.randomUUID().toString();
 
-    private static final String VISITOR_ID = UUID.randomUUID().toString();
+  // get search service client
+  private static SearchServiceClient getSearchServiceClient() throws IOException {
+    SearchServiceSettings settings = SearchServiceSettings.newBuilder()
+        .setEndpoint(ENDPOINT)
+        .build();
+    return SearchServiceClient.create(settings);
+  }
 
-    // [START search_client]
-    private static SearchServiceClient getSearchServiceClient() throws IOException {
-        SearchServiceSettings settings = SearchServiceSettings.newBuilder()
-                .setEndpoint(ENDPOINT)
-                .build();
-        return SearchServiceClient.create(settings);
-    }
-    // [END search_client]
+  // get search service request
+  public static SearchResponse searchProducts_withNextPageToken(String query, String pageToken)
+      throws IOException, InterruptedException {
+    SearchServiceClient searchClient = getSearchServiceClient();
 
-    private static final String DEFAULT_BRANCH_NAME =
-            DEFAULT_CATALOG_NAME + "/branches/default_branch";
+    SearchRequest firstRequest = SearchRequest.newBuilder()
+        .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
+        .setVisitorId(VISITOR_ID) // A unique identifier to track visitors
+        .setQuery(query)
+        .setPageToken(pageToken)
+        .build();
 
-    // [START search_product_page_token]
-    public static SearchResponse searchProducts_withNextPageToken(String query, int pageSize)
-            throws IOException, InterruptedException {
-        SearchServiceClient searchClient = getSearchServiceClient();
+    SearchResponse firstResponse = searchClient.search(firstRequest).getPage()
+        .getResponse();
 
-        SearchRequest firstRequest = SearchRequest.newBuilder()
-                .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
-                .setBranch(DEFAULT_BRANCH_NAME)
-                .setVisitorId(VISITOR_ID)
-                .setQuery(query)
-                .setPageSize(pageSize)
-                .build();
-        SearchResponse firstResponse = searchClient.search(firstRequest).getPage()
-                .getResponse();
+    SearchRequest secondRequest = SearchRequest.newBuilder()
+        .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
+        .setVisitorId(VISITOR_ID) // A unique identifier to track visitors
+        .setQuery(query)
+        .setPageToken(firstResponse.getNextPageToken())
+        .build();
 
-        SearchRequest secondRequest = SearchRequest.newBuilder()
-                .setPlacement(DEFAULT_SEARCH_PLACEMENT_NAME)
-                .setBranch(DEFAULT_BRANCH_NAME)
-                .setVisitorId(VISITOR_ID)
-                .setQuery(query)
-                .setPageSize(pageSize)
-                .setPageToken(firstResponse.getNextPageToken())
-                .build();
-        System.out.println("Search with pagination using page token, request: " + secondRequest);
-        SearchResponse response = searchClient.search(secondRequest).getPage().getResponse();
+    System.out.println("Search with pagination using page token, request: " + secondRequest);
 
-        searchClient.shutdownNow();
-        searchClient.awaitTermination(2, TimeUnit.SECONDS);
-        System.out.println("Search with pagination using page token, response: " + response);
-        return response;
-    }
-    // [END search_product_page_token]
+    SearchResponse response = searchClient.search(secondRequest).getPage().getResponse();
 
-    @Test
-    public void search() throws IOException, InterruptedException {
+    searchClient.shutdownNow();
+    searchClient.awaitTermination(2, TimeUnit.SECONDS);
 
-        searchProducts_withNextPageToken("Nest_Maxi", 10);
+    System.out.println("Search with pagination using page token, response: " + response);
+    return response;
+  }
 
-    }
+  // call the Retail Search
+  @Test
+  public void search() throws IOException, InterruptedException {
+    // TRY DIFFERENT PAGINATION PARAMETERS HERE:
+    String pageToken = "";
+
+    searchProducts_withNextPageToken("Hoodie", pageToken);
+  }
 }
+
+// [END retail_search_for_products_with_next_page]
